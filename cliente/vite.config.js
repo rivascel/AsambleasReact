d3b5501904a95dc4ae1f9979d1656ebbf918553c
Ivan <rivascel@gmail.com>
2026-01-07 08:50:59 -0500

    ajuste carpeta cliente

--- cliente/vite.config.js
+++ cliente/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react';
-import fs from 'fs';
+// import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -10,15 +10,15 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
   plugins: [react(),tailwindcss()],
   build: {
-    outDir: 'dist', // Esta es la opción que garantiza el nombre de la carpeta
+    outDir: 'dist',
   },
   server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, '../src/ssl/localhost-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, '../src/ssl/localhost.pem')),
-    },
+  //   https: {
+  //     key: fs.readFileSync(path.resolve(__dirname, '../src/ssl/localhost-key.pem')),
+  //     cert: fs.readFileSync(path.resolve(__dirname, '../src/ssl/localhost.pem')),
+  //   },
     port: 5173,
-    historyApiFallback: true
+    // historyApiFallback: true
   }
 });
 
